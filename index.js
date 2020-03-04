73458e9c47adf67e8e5194989422d005b1b8f104
Jaycee Bagtas <jaycee.bagtas@gmail.com>
2020-03-04 12:28:38 -0800

    started coding index.js

--- index.js
+++ index.js
@@ -1,1 +1,160 @@
 //path for js files is ./Develop/lib/
+const inquirer = require("inquirer");
+
+const render = require("./Develop/lib/htmlRenderer");
+const Manager = require("./Develop/lib/Manager");
+const Engineer = require("./Develop/lib/Engineer");
+const Intern = require("./Develop/lib/Intern");
+const Employee = require("./Develop/lib/Employee");
+
+const teamMembers = [];
+
+function addMember() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "type",
+            message: "Which type of team member would you like to add?",
+            choices: [
+                "Engineer",
+                "Intern",
+                "I don't want to add any more team members"
+            ]
+
+        }
+    ]).then(function(answer) {
+        if(answer.type === "Engineer") {
+            createEngineer();
+        }
+        else if (answer.type === "Intern") {
+            createIntern();
+        }
+        else {
+            render(teamMembers);
+        }
+
+    })
+}
+
+function createManager () {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your manager's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your manager's id?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your manager's email?"
+        },
+        {
+            type: "input",
+            name: "office",
+            message: "What is your manager's office number?"
+        }
+
+    ]).then(function (answers) {
+        const manager = new Manager(answers.name, parseInt(answers.id), answers.email, parseInt(answers.office));
+        teamMembers.push(manager);
+        addMember();
+    });
+}
+
+function createEngineer () {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your engineer's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your engineer's id?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your engineer's email?"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is your engineer's github?"
+        }
+
+    ]).then(function (answers) {
+        const engineer = new Engineer(answers.name, parseInt(answers.id), answers.email, answers.github);
+        teamMembers.push(engineer);
+        addMember();
+    });
+
+}
+
+function createIntern () {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your intern's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your intern's id?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your intern's email?"
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "What is your intern's school?"
+        }
+
+    ]).then(function (answers) {
+        const intern = new Intern(answers.name, parseInt(answers.id), answers.email, answers.school);
+        teamMembers.push(intern);
+        addMember();
+    });
+
+}
+function createEmployee () {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your manager's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your manager's id?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your manager's email?"
+        },
+        {
+            type: "input",
+            name: "office",
+            message: "What is your manager's office number?"
+        }
+
+    ]).then(function (answers) {
+        const employee = new Employee(answers.name, parseInt(answers.id), answers.email, parseInt(answers.office));
+        teamMembers.push(employee);
+        addMember();
+    });
+}
+
+createManager();
