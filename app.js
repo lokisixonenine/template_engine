424b3ff9b8d08898741d1134f58f4ff04dd5104d
Jaycee Bagtas <jaycee.bagtas@gmail.com>
2020-03-06 12:51:42 -0800

    added corrected app.js file per TA's instructions
    changed index.js filename to app.js filename

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const Engineer = require("./Develop/lib/Engineer");
 const Intern = require("./Develop/lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./Develop/lib/htmlRenderer");
 
 let teamArray = [];
 
